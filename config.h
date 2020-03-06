fbd94682f627f612a7ab817f07c95e5e365b588d
gk07 <gk_520@hotmail.com>
2020-03-06 21:45:42 +0800

    changer border and scratchpad settings

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -83,7 +83,7 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
                                   "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "urxvtc", NULL };
 static const char scratchpadname[] = "ScratchPad";
-static const char *scratchpadcmd[] = { "urxvtc", "-name", scratchpadname, "-geometry", "120x42", NULL };
+static const char *scratchpadcmd[] = { "urxvtc", "-name", scratchpadname, "-geometry", "90x42", NULL };
 
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
