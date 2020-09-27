2dbe416b476c914b6cec42bcfc4d660e9004c688
gk07 <gk_520@hotmail.com>
2020-09-27 16:43:17 +0800

    Add window rule for Dict

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -52,6 +52,7 @@ static const Rule rules[] = {
     { "URxvt",                 "pick emoji",    "fzf_emoji",    0,         1,          1,          -1 },
     { "Electron",              "electron",      NULL,           1 << 8,    1,          1,          -1 },
     { "code-oss",              "code-oss",      NULL,           1 << 8,    1,          1,          -1 },
+    { "Alacritty",             "Alacritty",     "Dict",         0,         1,          1,          -1 },
 };
 
 /* layout(s) */
